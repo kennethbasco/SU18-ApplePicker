ace306139fdc2e4d35b800073047479976d071f4
kennethbasco <kpb82@scarletmail.rutgers.edu>
2018-06-07 20:02:01 -0400

    Changed Camera, added movement

--- Apple Picker Prototype/Assets/Scripts/AppleTree.cs
+++ Apple Picker Prototype/Assets/Scripts/AppleTree.cs
@@ -2,15 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AppleTree : MonoBehaviour {
+public class AppleTree : MonoBehaviour 
+{
+
+	[Header("Set in Inspector")]
+
+	public GameObject applePrefab;
+	public float speed = 1f;
+	public float leftAndRightEdge = 10f;
+	public float chanceToChangeDirections = 0.1f;
+	public float secondsBetweenAppleDrops = 1f;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+
+		//Dropping Apples every second
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+
+		//Basic Movement
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+
+		//Changing Direction
+
+		if (pos.x < -leftAndRightEdge) {
+			speed = Mathf.Abs (speed); //Move Right
+
+		} 
+		else if (pos.x > leftAndRightEdge) 
+		{
+			speed = -Mathf.Abs (speed); //Move Left
+		} 
+		else if (Random.value < chanceToChangeDirections) 
+		{
+			speed *= -1; //Change Direction
+		}
 		
+	}
+
+	void FixedUpdate()
+	{
+		//change rando direction is time based
+		if (Random.value < chanceToChangeDirections) 
+		{
+			speed *= -1; //Change Direction
+		}
 	}
 }
