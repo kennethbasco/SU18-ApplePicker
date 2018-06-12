3c91bc3f57fba2896b29760b2226396e7e095794
kennethbasco <kpb82@scarletmail.rutgers.edu>
2018-06-12 10:51:53 -0400

    Duplicated Scene, added Basket Movement

--- Apple Picker Prototype/Assets/Scripts/Basket.cs
+++ Apple Picker Prototype/Assets/Scripts/Basket.cs
@@ -11,6 +11,33 @@ public class Basket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		//get current screen position forom mouse input
+		Vector3 mousePos2D = Input.mousePosition;
+		//camera's z position sets where mouse is into 3D space
+		mousePos2D.z = -Camera.main.transform.position.z;
+		//convert 2d screen to 3d world space
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint ( mousePos2D );
+
+		//move the x position of this Basket to the x position of the Mouse
+		Vector3 pos = this.transform.position;
+		pos.x = mousePos3D.x;
+		this.transform.position = pos;
+
+
+	}
+
+	void OnCollisionEnter( Collision coll )
+	{
+		//Find out what hit this basket
+
+		GameObject collidedWith = coll.gameObject;
+
+		if (collidedWith.tag == "Apple") {
+
+			Destroy (collidedWith);
+
+		}
+
 	}
 }
